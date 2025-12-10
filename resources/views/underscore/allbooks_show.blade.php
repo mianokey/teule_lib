0589bfbb6e8cef042922437fcf864917288db269
mianokey <mianokey@gmail.com>
2025-12-10 12:39:16 +0300

    edited books list

--- resources/views/underscore/allbooks_show.blade.php
+++ resources/views/underscore/allbooks_show.blade.php
@@ -1,8 +1,8 @@
 <tr>
     <td><%= obj.book_id %></td>
     <td title="Description: <%= obj.description %>"><%= obj.title %></td>
-    <td><%= obj.author %></td>
     <td><%= obj.ISBN %></td>
+    <td><%= obj.author %></td>
     <td><%= obj.category %></td>
     <td><a class="btn btn-success"><%= obj.avaliable %></a></td>
     <td><a class="btn btn-inverse"><%= obj.total_books %></a></td>
